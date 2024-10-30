04224eae046f3cb46729337b8a07d175ae550080
Slobodian17 <86010353+Slobodian17@users.noreply.github.com>
2024-10-31 01:56:29 +0200

    Update getEvents.js

--- src/pages/calendar/api/getEvents.js
+++ src/pages/calendar/api/getEvents.js
@@ -1,5 +1,5 @@
 export const getEvents = async () => {
-    const response = await fetch("http://13.60.196.234:5000/events", {
+    const response = await fetch("http://13.51.196.110:5000/events", {
         headers: {
             Authorization: localStorage.getItem("Authorization"),
             accept: "application/json",
