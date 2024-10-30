37f2a564732553ceab5d1c384a80535300b08449
Slobodian17 <86010353+Slobodian17@users.noreply.github.com>
2024-10-30 23:54:49 +0200

    Update getCalendars.js

--- src/pages/adminCalendars/api/getCalendars.js
+++ src/pages/adminCalendars/api/getCalendars.js
@@ -1,5 +1,5 @@
 export const getCalendars = async () => {
-    const response = await fetch("http://127.0.0.1:5000/calendars", {
+    const response = await fetch("http://13.60.196.234:5000/calendars", {
         headers: {
             Authorization: localStorage.getItem("Authorization"),
             accept: "application/json",
