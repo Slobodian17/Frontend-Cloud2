d88545ab9f83725f5b0c3aea35a965d971a0ddf2
Slobodian17 <86010353+Slobodian17@users.noreply.github.com>
2024-10-30 23:53:18 +0200

    Update deleteAccess.js

--- src/pages/adminAccesses/api/deleteAccess.js
+++ src/pages/adminAccesses/api/deleteAccess.js
@@ -1,6 +1,6 @@
 export const deleteAccess = async (user_id, calendar_id) => {
     try {
-        const response = await fetch(`http://127.0.0.1:5000/calendar/get_access/${+user_id}/${+calendar_id}`, {
+        const response = await fetch(`http://13.60.196.234:5000/calendar/get_access/${+user_id}/${+calendar_id}`, {
             method: "DELETE",
             headers: {
                 Authorization: localStorage.getItem("Authorization"),
