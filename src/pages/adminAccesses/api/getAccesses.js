d9cd9bdfe9159e5a9f9995499f22c650175e145b
Slobodian17 <86010353+Slobodian17@users.noreply.github.com>
2024-10-30 23:53:54 +0200

    Update getAccesses.js

--- src/pages/adminAccesses/api/getAccesses.js
+++ src/pages/adminAccesses/api/getAccesses.js
@@ -1,5 +1,5 @@
 export const getAccesses = async () => {
-    const response = await fetch("http://127.0.0.1:5000/access", {
+    const response = await fetch("http://13.60.196.234:5000/access", {
         headers: {
             Authorization: localStorage.getItem("Authorization"),
             accept: "application/json",
