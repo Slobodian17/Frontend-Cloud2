c0a06cbf6b84daa0be72251c971d2aa4349c9379
Slobodian17 <86010353+Slobodian17@users.noreply.github.com>
2024-10-30 23:55:32 +0200

    Update getCategories.js

--- src/pages/adminCategories/api/getCategories.js
+++ src/pages/adminCategories/api/getCategories.js
@@ -1,5 +1,5 @@
 export const getCategories = async () => {
-    const response = await fetch("http://127.0.0.1:5000/categories", {
+    const response = await fetch("http://13.60.196.234:5000/categories", {
         headers: {
             Authorization: localStorage.getItem("Authorization"),
             accept: "application/json",
