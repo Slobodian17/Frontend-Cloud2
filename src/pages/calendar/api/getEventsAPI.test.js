55a7bdeddcc7b811e5720f83308a131cb7c60554
Slobodian17 <86010353+Slobodian17@users.noreply.github.com>
2024-10-31 01:56:37 +0200

    Update getEventsAPI.test.js

--- src/pages/calendar/api/getEventsAPI.test.js
+++ src/pages/calendar/api/getEventsAPI.test.js
@@ -14,7 +14,7 @@ describe('getEvents', () => {
     it('fetches events with correct headers', async () => {
         await getEvents();
 
-        expect(global.fetch).toHaveBeenCalledWith('http://127.0.0.1:5000/events', {
+        expect(global.fetch).toHaveBeenCalledWith('http://13.51.196.110:5000/events', {
             headers: {
                 Authorization: localStorage.getItem('Authorization'),
                 accept: 'application/json',
