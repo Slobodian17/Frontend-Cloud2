eed2e15147988d29cf470889080ce66a2d4b4196
Slobodian17 <86010353+Slobodian17@users.noreply.github.com>
2024-10-31 01:58:32 +0200

    Update createEvent.js

--- src/pages/createEvent/api/createEvent.js
+++ src/pages/createEvent/api/createEvent.js
@@ -1,6 +1,6 @@
 export const createEvent = async (event, id) => {
     try {
-        const response = await fetch(`http://127.0.0.1:5000/event/${id}`, {
+        const response = await fetch(`http://13.51.196.110:5000/event/${id}`, {
             method: "POST",
             body: JSON.stringify(event),
             headers: {
