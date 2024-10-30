00cd7a94fe821dd5d06ccf2d5402d909fb9ab92b
Slobodian17 <86010353+Slobodian17@users.noreply.github.com>
2024-10-31 01:57:30 +0200

    Update createCalendar.js

--- src/pages/createCalendar/api/createCalendar.js
+++ src/pages/createCalendar/api/createCalendar.js
@@ -1,6 +1,6 @@
 export const createCalendar = async (calendar) => {
     try {
-        const responce = await fetch("http://127.0.0.1:5000/calendar", {
+        const responce = await fetch("http://13.51.196.110:5000/calendar", {
             method: "POST",
             body: JSON.stringify(calendar),
             headers: {
