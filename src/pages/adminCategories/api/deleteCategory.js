19269107901cc58b86a2d30326cca6772e9aec5d
Slobodian17 <86010353+Slobodian17@users.noreply.github.com>
2024-10-31 01:53:18 +0200

    Update deleteCategory.js

--- src/pages/adminCategories/api/deleteCategory.js
+++ src/pages/adminCategories/api/deleteCategory.js
@@ -1,6 +1,6 @@
 export const deleteCategory = async (id) => {
     try {
-        const response = await fetch(`http://13.60.196.234:5000/category/${id}`, {
+        const response = await fetch(`http://13.51.196.110:5000/category/${id}`, {
             method: "DELETE",
             headers: {
                 Authorization: localStorage.getItem("Authorization"),
