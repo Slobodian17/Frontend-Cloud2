aaad522725d1499f19c64d26571c9934263e066d
Slobodian17 <86010353+Slobodian17@users.noreply.github.com>
2024-10-31 01:53:27 +0200

    Update deleteCategoryAPI.test.js

--- src/pages/adminCategories/api/deleteCategoryAPI.test.js
+++ src/pages/adminCategories/api/deleteCategoryAPI.test.js
@@ -13,7 +13,7 @@ describe('deleteCategory', () => {
 
         await deleteCategory(id);
 
-        expect(fetchMock).toHaveBeenCalledWith(`http://13.60.196.234:5000/category/${id}`, {
+        expect(fetchMock).toHaveBeenCalledWith(`http://13.51.196.110:5000/category/${id}`, {
             method: 'DELETE',
             headers: {
                 Authorization: 'fakeToken',
