f61db8d136ace6c72ce4e46b6b547575cd0edf6e
Slobodian17 <86010353+Slobodian17@users.noreply.github.com>
2024-10-31 02:01:42 +0200

    Update getEventById.js

--- src/pages/updateEvent/api/getEventById.js
+++ src/pages/updateEvent/api/getEventById.js
@@ -1,6 +1,6 @@
 export const getEventById = async (id) => {
     try {
-        const response = await fetch(`http://127.0.0.1:5000/event/${id}`, {
+        const response = await fetch(`http://13.51.196.110:5000/event/${id}`, {
             method: "GET",
             headers: {
                 Authorization: localStorage.getItem("Authorization"),
