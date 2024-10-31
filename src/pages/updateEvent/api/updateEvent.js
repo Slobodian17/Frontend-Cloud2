56d6ff42cfe3f4eeb852701c00f70e652112c9fb
Slobodian17 <86010353+Slobodian17@users.noreply.github.com>
2024-10-31 02:01:58 +0200

    Update updateEvent.js

--- src/pages/updateEvent/api/updateEvent.js
+++ src/pages/updateEvent/api/updateEvent.js
@@ -1,6 +1,6 @@
 export const updateEvent = async (event, id) => {
     try {
-        const response = await fetch(`http://127.0.0.1:5000/event/${id}`, {
+        const response = await fetch(`http://13.51.196.110:5000/event/${id}`, {
             method: "PUT",
             body: JSON.stringify(event),
             headers: {
